3137bb469770858d00b7ffbd196b145100293952
marco.madalin.farcas <marco.madalin.farcas@est.fib.upc.edu>
2023-07-09 11:45:44 +0200

    added cors config

--- server.js
+++ server.js
@@ -5,29 +5,31 @@ const mongoose = require("mongoose");
 const twittRoutes = require("./routes/twittRoutes");
 const userRoutes = require("./routes/userRoutes");
 const multer = require('multer');
+const connectDB = require("./config/dbCon");
+const corsOptions = require("./config/corsOptions");
 
 const app = express();
 
+connectDB()
+
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(cors());
 app.use(multer().any())
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   console.log(req.body);
   next();
 });
 
+
 app.use("/twitts", twittRoutes);
 app.use("/users", userRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then((result) => {
-    //port
+
+mongoose.connection.once('open', () => {
     app.listen(process.env.PORT, () => {
-      console.log("Connected to DB, listening on port: " + process.env.PORT);
+        console.log("Connected to DB, listening on port: " + process.env.PORT);
     });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+})
