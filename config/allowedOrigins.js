729101c7f2c3702de0e83fc71d284fdc7fe4d25d
marco.madalin.farcas <marco.madalin.farcas@est.fib.upc.edu>
2023-07-12 16:40:24 +0200

    added origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'https://www.twotter.onrender.com',
     "https://twotter-owma.onrender.com",
+    "https://twootterr.netlify.app"
 ]
 
 module.exports = allowedOrigins
