5985bcc32daa69e56e060f6d7e71d8b0536b6f2a
marco.madalin.farcas <marco.madalin.farcas@est.fib.upc.edu>
2023-09-07 17:13:12 +0200

    LAST COMMIT FINALLY!!

--- config/dbCon.js
+++ config/dbCon.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI)
     } catch (err) {
-        console.log(err)
+         console.log(err)
     }
 }
 
